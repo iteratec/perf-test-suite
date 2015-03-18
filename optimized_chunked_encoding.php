64dbad9f3cc655a3dd4777010eeff0579d29d666
Thomas Heigl <thomas.heigl@iteratec.de>
2015-03-18 13:01:20 +0100

    Fixed Js-non-minified version

--- optimized_chunked_encoding.php
+++ optimized_chunked_encoding.php
@@ -33,9 +33,9 @@
 
     <!-- jQuery Version 1.11.0 -->
 	<script src="js/jquery-1.11.0.js"></script>
-	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jquery-ui.js"></script>
 	<!-- Bootstrap Core JavaScript -->
-	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap.js"></script>
 	<script src="js/jquery.magnific-popup.js"></script>
 	<script src="js/shuffle.js"></script>
 	<script src="js/jquery.shapeshift.js"></script>
