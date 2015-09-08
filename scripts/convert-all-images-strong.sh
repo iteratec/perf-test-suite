f427275c0a78f9dcfae171320641df660872851a
Nils Kuhn <nils.kuhn@iteratec.de>
2015-09-08 13:57:28 +0200

    added variant with strongly compressed images

---
+++ scripts/convert-all-images-strong.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+IMAGES_ORIGINAL=../assets/images/*.*
+IMAGES_OPTIMIZED=../assets/images_optimized_strong/*.*
+
+# copy original images
+for f in $IMAGES_ORIGINAL
+do
+	cp -f $f ../assets/images_optimized_strong
+done
+# compress all images
+for f in $IMAGES_OPTIMIZED
+do
+	echo "converting $f"
+	convert -strip -quality 5% $f $f
+done
